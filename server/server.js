81fd92fd7f7b8a8d7249ccd8484864ecffb29a32
Dhruv Mishra <dhruv.mishra@sezzle.com>
2018-11-25 17:56:14 +0530

    node-chat with webelves

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 
 const publicPath = path.join(__dirname,"../public")
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 var app = express();
 app.use(express.static(publicPath))
